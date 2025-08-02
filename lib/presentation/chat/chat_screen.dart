b478a97b633740410304217b37d5ebd8d8ce1e79
camiloaacevedo <ccuevasa@uninorte.edu.co>
2025-08-02 02:30:26 -0500

    Chat screen

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://images2.fanpop.com/image/photos/13700000/-Young-Princess-avril-lavigne-13727592-1000-1535.jpg',
+            ),
+          ),
+        ),
+        title: Text('Mi amor ♥'),
+      ),
+    );
+  }
+}
